6e7f9cf84278935a6d9623f29e5f9c5ac1b0627e
Rosa <uo277921@uniovi.es>
2022-02-07 10:28:14 +0100

    SDI-2122-909-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2122909spring/Sdi2122909SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122909spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2122909SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2122909SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2122909spring/Sdi2122909SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122909spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2122909SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
