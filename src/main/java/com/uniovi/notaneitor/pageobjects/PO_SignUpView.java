548923acf019cdbaa98ac0d6bcdec456f450c7a4
Rosa <uo277921@uniovi.es>
2022-03-02 18:49:01 +0100

    SDI-2122-909-5.5-Casos Uso VistaSignUp.

---
+++ src/main/java/com/uniovi/notaneitor/pageobjects/PO_SignUpView.java
@@ -0,0 +1,39 @@
+package com.uniovi.notaneitor.pageobjects;
+
+import com.uniovi.notaneitor.util.SeleniumUtils;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class PO_SignUpView extends PO_NavView {
+
+    static public void fillForm(WebDriver driver, String dnip, String namep, String lastnamep, String
+            passwordp, String passwordconfp) {
+        WebElement dni = driver.findElement(By.name("dni"));
+        dni.click();
+        dni.clear();
+        dni.sendKeys(dnip);
+        WebElement name = driver.findElement(By.name("name"));
+        name.click();
+        name.clear();
+        name.sendKeys(namep);
+        WebElement lastname = driver.findElement(By.name("lastName"));
+        lastname.click();
+        lastname.clear();
+        lastname.sendKeys(lastnamep);
+        WebElement password = driver.findElement(By.name("password"));
+        password.click();
+        password.clear();
+        password.sendKeys(passwordp);
+        WebElement passwordConfirm = driver.findElement(By.name("passwordConfirm"));
+        passwordConfirm.click();
+        passwordConfirm.clear();
+        passwordConfirm.sendKeys(passwordconfp);
+        //Pulsar el boton de Alta.
+        By boton = By.className("btn");
+        driver.findElement(boton).click();
+    }
+}
